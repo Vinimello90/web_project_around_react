dc7399e104e8db9bb6f81e13e8ad9ba5f78a8ac2
Vinimello90 <vinicius.barretto9022@gmail.com>
2025-02-22 15:12:37 -0300

    feat: add an API class and useState to retrieve the cards data

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,31 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { api } from "../../utils/Api";
 import Popup from "./components/Popup/Popup";
 import NewCard from "./components/Popup/components/NewCard/NewCard";
 import EditProfile from "./components/Popup/components/EditProfile/EditProfile";
 import EditAvatar from "./components/Popup/components/EditAvatar/EditAvatar";
 import Card from "./components/Card/Card";
 
-const cards = [
-  {
-    isLiked: false,
-    _id: "5d1f0611d321eb4bdcd707dd",
-    name: "Yosemite Valley",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
-    owner: "5d1f0611d321eb4bdcd707dd",
-    createdAt: "2019-07-05T08:10:57.741Z",
-  },
-  {
-    isLiked: false,
-    _id: "5d1f064ed321eb4bdcd707de",
-    name: "Lake Louise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
-    owner: "5d1f0611d321eb4bdcd707dd",
-    createdAt: "2019-07-05T08:11:58.324Z",
-  },
-];
-
 export default function Main() {
   const [popup, setPopup] = useState("");
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    api.getInitialCards().then((cards) => setCards(cards));
+  }, []);
 
   const editAvatarPopup = {
     title: "Alterar a foto do perfil",
@@ -39,12 +26,10 @@ export default function Main() {
 
   function handleOpenPopup(popup2) {
     setPopup(popup2);
-    console.log(popup2);
   }
 
   function handleClosePopup() {
     setPopup("");
-    console.log(popup === true);
   }
 
   return (
---
+++ src/utils/Api.js
@@ -0,0 +1,101 @@
+// Classe Api é responsável por manipular os dados enviados e recebidos do perfil e dos cartões pela API
+class Api {
+  constructor(options) {
+    // Constructor recebe o options do fetch como parâmetro
+    this._baseUrl = options.baseUrl;
+    this._headers = options.headers;
+  }
+
+  _checkResponse(res) {
+    return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
+  }
+
+  // Método público getInitialCards() é responsável por enviar a solicitação das informações dos cartões utilizando e retornando o método fetch com o método de solicitação "GET"
+  getInitialCards() {
+    return fetch(`${this._baseUrl}/cards`, {
+      headers: {
+        authorization: this._headers.authorization,
+      },
+    })
+      .then(this._checkResponse)
+      .catch((err) => console.log(err));
+  }
+
+  // Método público addNewCard() é responsável por enviar a solicitação dos dados do novo cartão adicionado utilizando e retornando o método fetch com o método de solicitação "POST"
+  addNewCard(data) {
+    return fetch(`${this._baseUrl}/cards/`, {
+      method: "POST",
+      headers: this._headers,
+      body: JSON.stringify(data),
+    })
+      .then(this._checkResponse)
+      .catch((err) => console.log(err));
+  }
+
+  // Método público editLikeStatus() é responsável por enviar a solicitação para alterar o status do like utilizando e retornando o método fetch com os métodos de solicitações "PUT"
+  // para adicionar "true" e "DELETE" para remover mudando para "false".
+  editLikeStatus({ method, id }) {
+    return fetch(`${this._baseUrl}/cards/${id}/likes`, {
+      method: method,
+      headers: {
+        authorization: this._headers.authorization,
+      },
+    })
+      .then(this._checkResponse)
+      .catch((err) => console.log(err));
+  }
+
+  // Método público deleteCard() é responsável por enviar a solicitação para remover o card através do id do cartão utilizando e retornando o método fetch com o método de solicitação "DELETE".
+  deleteCard(id) {
+    return fetch(`${this._baseUrl}/cards/${id}`, {
+      method: "DELETE",
+      headers: {
+        authorization: this._headers.authorization,
+      },
+    })
+      .then(this._checkResponse)
+      .catch((err) => console.log(err));
+  }
+
+  // Método público getUserInfo() é responsável por enviar a solicitação das informações do perfil do usuário utilizando e retornando o método fetch com o método de solicitação "GET"
+  getUserInfo() {
+    return fetch(`${this._baseUrl}/users/me`, {
+      headers: {
+        authorization: this._headers.authorization,
+      },
+    })
+      .then(this._checkResponse)
+      .catch((err) => console.log(err));
+  }
+
+  // Método público editUserAvatar() é responsável  por enviar a solicitação da alteração do link da imagem do avatar do perfil do usuário utilizando e retornando o método fetch com o método
+  // de solicitação "PATCH"
+  updateUserAvatar(data) {
+    return fetch(`${this._baseUrl}/users/me/avatar`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify(data),
+    })
+      .then(this._checkResponse)
+      .catch((err) => console.log(err));
+  }
+
+  // Método público editUserInfo() é responsável  por enviar a solicitação da alteração das informações do perfil do usuário utilizando e retornando o método fetch com o método de solicitação "PATCH"
+  updateUserInfo(data) {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify(data),
+    })
+      .then(this._checkResponse)
+      .catch((err) => console.log(err));
+  }
+}
+
+export const api = new Api({
+  baseUrl: "https://around-api.pt-br.tripleten-services.com/v1",
+  headers: {
+    authorization: "3104c43f-5c52-4781-879f-672ac8ed2b72",
+    "Content-Type": "application/json",
+  },
+});
