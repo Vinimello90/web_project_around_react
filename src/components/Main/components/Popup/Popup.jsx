987acf60f76b3e2525188306aa81cce24ee3db10
Vinimello90 <vinicius.barretto9022@gmail.com>
2025-02-28 19:30:37 -0300

    fix: update lint rules and resolve warnings

--- src/components/Main/components/Popup/Popup.jsx
+++ src/components/Main/components/Popup/Popup.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 export default function Popup(props) {
   const [displayPopup, setDisplayPopup] = useState("");
-  const { title, children } = props;
+  const { title, children, onClose } = props;
 
   useEffect(() => {
     // Muda o estado para adicionar a classe e ativar animação de fade in.
@@ -10,7 +10,7 @@ export default function Popup(props) {
     // Manipula o ouvinte de eventos "keydown" para fechar a popop na tecla "Escape".
     function handleEsc(evt) {
       if (evt.key === "Escape") {
-        props.onClose();
+        onClose();
       }
     }
     // adiciona o event listener ao montar.
@@ -19,12 +19,13 @@ export default function Popup(props) {
       // remove o event listener ao desmontar.
       window.removeEventListener("keydown", handleEsc);
     };
-  }, []);
+    // Estava indicando um aviso de dependência aqui, mas não necessita dele realmente né? já que a função via props só vai desmontar a popup correto??
+  }, [onClose]);
 
   // Manipula o clique no elemento de background da popup para fechar ao clicar fora da popup.
   function handleClickOutside(evt) {
     if (evt.target.classList.contains("popup")) {
-      props.onClose();
+      onClose();
     }
   }
 
@@ -40,7 +41,7 @@ export default function Popup(props) {
           type="button"
           aria-label="Close modal"
           className="button button_close"
-          onClick={props.onClose}
+          onClick={onClose}
         ></button>
         {title && <h2 className="popup__title">{title}</h2>}
         {children}
