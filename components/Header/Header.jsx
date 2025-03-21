1e0fab2a7bd1fa492cfab8a1243ec00f9b3a13f9
vinimello90 <vinicius.barretto9022@gmail.com>
2025-03-21 10:30:20 -0300

    Updates

--- components/Header/Header.jsx
+++
@@ -1,9 +0,0 @@
-import aroundLogo from "../../images/logo.svg";
-
-export default function Header() {
-  return (
-    <header className="header">
-      <img src={aroundLogo} alt="logotipo EUA Afora" className="header__logo" />
-    </header>
-  );
-}
--- components/Main/components/NoCards/NoCards.jsx
+++
@@ -1,7 +0,0 @@
-export default function NoCards() {
-  return (
-    <li className="no-cards">
-      <p className="no-cards__text">Ainda não há cartões</p>
-    </li>
-  );
-}
